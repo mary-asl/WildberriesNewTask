83511b02c24c7a13ac02a206413e4a331040a2ef
Mariya Kazakova <Mariya_Kazakova@epam.com>
2020-09-06 14:36:38 +0600

    delete refresh() from methods which verify filters

--- src/main/java/pageObject/CategoryPage.java
+++ src/main/java/pageObject/CategoryPage.java
@@ -58,14 +58,12 @@ public class CategoryPage extends AbstractPage {
     }
 
     public CategoryPage filterByPrice() {
-        refresh();
         highlightElement(FILTER_BY_PRICE_BTN_LOCATOR);
         click(FILTER_BY_PRICE_BTN_LOCATOR);
         return this;
     }
 
     public CategoryPage filterByDiscount() {
-        refresh();
         highlightElement(FILTER_BY_DISCOUNT_BTN_LOCATOR);
         click(FILTER_BY_DISCOUNT_BTN_LOCATOR);
         return this;
